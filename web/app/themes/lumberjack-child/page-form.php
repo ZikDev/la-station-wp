7c787c9bf89813773fbf6b2b417c40f1d0a3f3b5
ZikDev <victor.bertschy@studentfr.ch>
2026-03-16 10:00:59 +0100

    Form v1

---
+++ web/app/themes/lumberjack-child/page-form.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+* Template Name: Formulaire de postulation
+*/
+
+namespace App;
+
+use App\Http\Controllers\Controller;
+use Rareloop\Lumberjack\Http\Responses\TimberResponse;
+use Rareloop\Lumberjack\Post;
+use Timber\Timber;
+
+class PageFormController extends Controller
+{
+    public function handle()
+    {
+        $context = Timber::get_context();
+        $post = new Post();
+
+        $context['post'] = $post;
+        $context['title'] = $post->title;
+        $context['content'] = $post->content;
+
+        return new TimberResponse('templates/form.twig', $context);
+    }
+}
